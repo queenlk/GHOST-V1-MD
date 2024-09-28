1dc27dbdbb31f68df76116e9dceca0bfe3ceb8cf
DENZO-UCHIWA <145277093+DENZO-UCHIWA@users.noreply.github.com>
2024-09-28 18:40:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ZP10BRrK#GRY8au51IjTAaa_NDQ1oB-h49thmrzkjcXchwBi0ECU",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/RSmYfMy/maxresdefault.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/5ba61f51e5dcda135e89d.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi BLAST-MD Is Online Now 💻\n*💻 Owner* - BLAST-MD\n\n*💻 Owner Number* -244956100349",
 SUDO_NB: process.env.SUDO_NB || "244956100349",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
