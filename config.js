6a1e928518eb5f1a000066455bec893512583222
DENZO-UCHIWA <145277093+DENZO-UCHIWA@users.noreply.github.com>
2024-10-03 09:53:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NnlFUYxL#Ye4oJ0VDNGIATtGL6bpQTFJBwWF3GPrB4urS-fVgyv0",
+SESSION_ID: process.env.SESSION_ID || "5Tcz1boQ#k9sOjLb65P3jvu5BBQ62vw6jfIri8K6j_1LUqRoyJEs",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/5ba61f51e5dcda135e89d.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi BLAST-MD Is Online Now 💻\n*💻 Owner* - BLAST-MD\n\n*💻 Owner Number* -244956100349",
