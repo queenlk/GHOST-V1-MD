c54b255776fc6445dade35a0e033d0be5fc4c95a
GHOST MODZ <gamingpissa530@gmail.com>
2024-10-17 15:41:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KVcTkKbb#nv_xLie9wc7--1uBUpNW9508QBndaA_WYAqbA7RtPkA",
+SESSION_ID: process.env.SESSION_ID || "PCIGiKQR#PCiw4EmmScTwP9XElhK1KN4I1ZOGjN4H63J6MtoUVY0",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* 94741140620",
